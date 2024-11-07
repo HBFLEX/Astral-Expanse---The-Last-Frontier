94e9039f565a85c00821e95c15ea4ef368c80382
HBFL3Xx <happybanda@dyuni.ac.mw>
2024-11-07 12:26:55 +0200

    add player running animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,17 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private PlayerControls _playerControls;
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private SpriteRenderer _sr;
     [SerializeField] private float _speed = 5f;
+    private Vector2 _movement;
 
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _rb = GetComponent <Rigidbody2D>();
+        _animator = GetComponent <Animator>();
+        _sr = GetComponent <SpriteRenderer>();
     }
 
     private void Start()
@@ -21,10 +27,42 @@ public class PlayerController : MonoBehaviour
         _playerControls.Enable();
     }
 
+    private void Update()
+    {
+        GetPlayerInput();
+        FixPlayerSize();
+        ChangePlayerDirectionOnAxis();
+    }
+
+    private void FixPlayerSize()
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
     private void MovePlayer()
     {
-        Vector2 movement = _playerControls.Player.Move.ReadValue<Vector2>();
-        _rb.MovePosition(_rb.position + movement * (_speed * Time.fixedDeltaTime));
+        _rb.MovePosition(_rb.position + _movement * (_speed * Time.fixedDeltaTime));
+    }
+
+    private void GetPlayerInput()
+    {
+        // get the player input
+        _movement = _playerControls.Player.Move.ReadValue<Vector2>();
+        // animate running state
+        _animator.SetFloat("MoveX", _movement.x);
+        _animator.SetFloat("MoveY", _movement.y);
+    }
+
+    private void ChangePlayerDirectionOnAxis()
+    {
+        if (_movement.x > 0.1)
+        {
+           _sr.flipX = true; 
+        }
+        else
+        {
+            _sr.flipX = false;
+        }
     }
 
     private void FixedUpdate()
