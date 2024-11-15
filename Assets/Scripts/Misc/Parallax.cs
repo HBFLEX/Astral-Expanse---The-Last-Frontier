6035ca17605a505dd95ac27065fc4f6daaa73806
HBFL3Xx <happybanda@dyuni.ac.mw>
2024-11-16 00:43:12 +0200

    feat: add background parallax effect

--- Assets/Scripts/Misc/Parallax.cs
+++ Assets/Scripts/Misc/Parallax.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera _cam;
+    private Vector2 _startPos;
+    private Vector2 travel => (Vector2)_cam.transform.position - _startPos;
+    [SerializeField] private float parallaxOffset = -0.4f;
+
+    private void Awake()
     {
-        
+        _cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _startPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = _startPos + (travel * parallaxOffset);
     }
 }
