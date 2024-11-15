bd76a612fd7193aa408732f3424c920e56e8fa84
HBFL3Xx <happybanda@dyuni.ac.mw>
2024-11-16 00:14:32 +0200

    feat: add transparency to vegetation when player is within bounds

--- Assets/Scripts/Misc/Transparency.cs
+++ Assets/Scripts/Misc/Transparency.cs
@@ -7,6 +7,7 @@ using UnityEngine.Tilemaps;
 public class Transparency : MonoBehaviour
 {
     private Tilemap _tilemap;
+    private SpriteRenderer _spriteRenderer;
     
     // fade effect options
     [Range(0, 1f)]
@@ -16,13 +17,15 @@ public class Transparency : MonoBehaviour
     private void Awake()
     {
         _tilemap = GetComponent<Tilemap>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(FadeRoutine(_tilemap, _tilemap.color.a, transparencyAmount, durationTime));
+            if(_tilemap != null) StartCoroutine(FadeRoutine(_tilemap, _tilemap.color.a, transparencyAmount, durationTime));
+            else if (_spriteRenderer != null) StartCoroutine(FadeRoutine(_spriteRenderer, _spriteRenderer.color.a, transparencyAmount, durationTime));
         }
     }
 
@@ -30,7 +33,9 @@ public class Transparency : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(FadeRoutine(_tilemap, _tilemap.color.a, 1f, durationTime));
+            if(_tilemap != null) StartCoroutine(FadeRoutine(_tilemap, _tilemap.color.a, 1f, durationTime));
+            else if(_spriteRenderer != null) StartCoroutine(FadeRoutine(_spriteRenderer, _spriteRenderer.color.a, 1f, durationTime));
+
         }
     }
 
@@ -42,6 +47,18 @@ public class Transparency : MonoBehaviour
             elapsedTime += Time.deltaTime;
             float newAlpha = Mathf.Lerp(startValue, endValue, elapsedTime / fadeTime);
             tilemap.color = new Color(tilemap.color.r, tilemap.color.g, tilemap.color.b, newAlpha);
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeRoutine(SpriteRenderer spriteRenderer, float startValue, float endValue, float fadeTime)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startValue, endValue, elapsedTime / fadeTime);
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, newAlpha);
             yield return null;
         }
     }
